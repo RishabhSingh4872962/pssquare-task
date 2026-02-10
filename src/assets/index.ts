40b3ebb507ebdbc91b48ee633107d1bce1e3bf42
Rishabh Singh <rs4872962@gmail.com>
2026-02-10 10:00:53 +0530

    add package and setup assets

---
+++ src/assets/index.ts
@@ -0,0 +1,41 @@
+import "../assets/dashboard/autumncollection.jpg";
+import "../assets/signup/applogo.png";
+
+import "../assets/dashboard/hangout.png";
+
+import "../assets/dashboard/product1.jpg";
+import "../assets/dashboard/product2.png";
+import "../assets/dashboard/product3.png";
+
+import "../assets/dashboard/recommended1.png";
+import "../assets/dashboard/recommended2.png";
+
+import "../assets/dashboard/topcollection1.png";
+import "../assets/dashboard/topcollection2.png";
+import "../assets/dashboard/topcollection3.png";
+import "../assets/dashboard/topcollection4.png";
+
+export const Images = {
+  dashboard: {
+    autumncollection: "../assets/dashboard/autumncollection.jpg",
+    hangout: "../assets/dashboard/hangout.png",
+  },
+  signup: {
+    applogo: "../assets/signup/applogo.png",
+  },
+  product: {
+    product1: "../assets/dashboard/product1.jpg",
+    product2: "../assets/dashboard/product2.png",
+    product3: "../assets/dashboard/product3.png",
+  },
+  recommended: {
+    recommended1: "../assets/dashboard/recommended1.png",
+    recommended2: "../assets/dashboard/recommended2.png",
+  },
+  topcollection: {
+    topcollection1: "../assets/dashboard/topcollection1.png",
+    topcollection2: "../assets/dashboard/topcollection2.png",
+    topcollection3: "../assets/dashboard/topcollection3.png",
+    topcollection4: "../assets/dashboard/topcollection4.png",
+  },
+};
